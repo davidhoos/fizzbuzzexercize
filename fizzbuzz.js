ef8cb1c6eae7defbbbdd2db08063b2fb6f506f6a
davidhoos <david.c.hoos@gmail.com>
2015-02-19 11:14:54 -0800

    completed exersize

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,23 @@
-for (var i = 1; i < 101; i++) {
+for (var countUp = 1; countUp < 101; countUp++) {
 	var divByThree = 3;
 	var divByFive = 5;
-		if (i % divByThree == 0) {
-			if (i % divByFive == 0) {
+
+	//Is the number divisible by three?
+	if (countUp % divByThree == 0) {
+
+		//Also, is it divisible by five?
+		if (countUp % divByFive == 0) {
+
+			//If so, then print fizzbuzz.
 			console.log("fizzbuzz");
-		} else {
+	//If its divisitble by three but not by five, print fizz.
+	} else 
 			console.log("fizz");
-		} else if (i % divByFive ==0) {
-			console.log("buzz")
-		} else {
-			console.log(i);
-		}
+	
+	//Is it divisible by five?
+	} else if (countUp % divByFive == 0) {
+			console.log("buzz");
+	} else {
+			console.log(countUp);
 	}
+}
